378bc5288f73a92c1d1df27ee5931e939f9a3af5
casey <caseymwangi31@gmail.com>
2024-04-09 13:11:54 +0300

    understanding fetch

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData (nameInput,emailInput) {
+   const formData = {
+    name: nameInput,
+    email: emailInput
+   };
+
+   const configObj = {
+    method : "POST",
+    headers : {
+        "content-Type" : "application/json",
+        "Accept": "application/json",
+    },
+     body: JSON.stringify(formData),
+    };
+    return fetch ("http://localhost:3000/users", configObj)
+    .then(res => res.json())
+    .then(data => {
+       document.body.innerHTML += `<p>user ID: ${data.id}</p>`;
+    })
+    .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+    
+};
+
